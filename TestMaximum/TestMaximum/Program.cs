86d48518f65cedf8af757fe2c311ebe2171fc97f
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-05 23:02:54 +0530

    [Roshni]-Finding Maximum of Float value

--- TestMaximum/TestMaximum/Program.cs
+++ TestMaximum/TestMaximum/Program.cs
@@ -4,14 +4,19 @@ using TestMaximum;
 Console.WriteLine("Welcome to Test Maximum");
 while (true)
 {
-    Console.WriteLine("PLease choose the option :\n1)Maximum of three Integers\n2)Maximum of three Float values\n3) Maximum of three Strings");
+    Console.WriteLine("Please Choose the Option :\n1)Maximum of three Integers\n2)Maximum of three Float values\n3) Maximum of three Strings");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
         case 1:
-            FindingMax obj1 = new FindingMax();
-            int maxValueInt = obj1.MaxValueInt(40, 20, 10);
+            FindingMax test1 = new FindingMax();
+            int maxValueInt = test1.MaxValueInt(40, 20, 10);
             Console.WriteLine("Maximum value when comparing three integers is " + maxValueInt);
+            break;
+        case 2:
+            FindingMaxofFloat test2 = new FindingMaxofFloat();
+            double MaxValueFloat = test2.MaxValueFloat(4.55, 2.45, 1.05);
+            Console.WriteLine("Maximum value when comparing three integers is " + MaxValueFloat);
             break;
         default:
             Console.WriteLine("please choose a correct option");
---
+++ TestMaximum/TestMaximumTests/FindingMaxofFloatTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestMaximum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum.Tests
+{
+    [TestClass()]
+    public class FindingMaxofFloatTests
+    {
+        [TestMethod()]
+        public void MaxValueIntTest()
+        {
+            FindingMaxofFloat find = new FindingMaxofFloat();
+            double  x = find.MaxValueFloat(4.55, 2.45, 1.05);
+            Assert.AreEqual(4.55, x);
+        }
+        [TestMethod()]
+        public void MaxValueIntTest2()
+        {
+            FindingMaxofFloat find = new FindingMaxofFloat();
+            double x = find.MaxValueFloat(2.45, 4.55, 1.05);
+            Assert.AreEqual(4.55, x);
+        }
+        [TestMethod()]
+        public void MaxValueIntTest3()
+        {
+            FindingMaxofFloat find = new FindingMaxofFloat();
+            double x = find.MaxValueFloat(1.05, 2.45, 4.55);
+            Assert.AreEqual(4.55, x);
+        }
+    }
+}
